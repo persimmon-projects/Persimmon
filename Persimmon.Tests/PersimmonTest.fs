996b1697e8e22630556dd20b8ca9130535fba08f
pocketberserker <pocketberserker@gmail.com>
2014-10-23 13:32:56 +0900

    initial implementation

---
+++ Persimmon.Tests/PersimmonTest.fs
@@ -0,0 +1,48 @@
+﻿namespace Parsimmon.Tests
+
+open Persimmon
+open NUnit.Framework
+open FsUnit
+
+[<TestFixture>]
+module PersimmonTest =
+
+  let shouldSucceed expected = function
+    | Success actual -> actual |> should equal expected
+    | Failure xs -> Assert.Fail(sprintf "%A" xs)
+
+  let shouldFail (expectedMessage: NonEmptyList<string>) = function
+    | Success x -> Assert.Fail(sprintf "Expect: Failure\nActual: %A" x)
+    | Failure actual -> actual |> should equal expectedMessage
+
+  [<Test>]
+  let ``simple succes assertion should succceed`` () =
+    test "simple success assertion should succee" {
+      return! success 1
+    }
+    |> shouldSucceed 1
+
+  [<Test>]
+  let ``simple failure asseertion should fail`` () =
+    let msg = "always fail"
+    test "simple failure assertion should fail" {
+      return! failure msg
+    }
+    |> shouldFail (msg, [])
+
+  [<Test>]
+  let ``all unit type assertion should run`` () =
+    test "all unit type assertion should run" {
+      do! assertEquals 1 2
+      do! assertEquals 2 2
+      do! assertEquals 3 4
+    }
+    |> shouldFail ("Expect: 1\nActual: 2", [ "Expect: 3\nActual: 4" ])
+
+  [<Test>]
+  let ``dependent assertion should not run if before assertion failed`` () =
+    test "dependent assertion should not run if before assertion failed" {
+      let! v = check 1 2
+      do! assertEquals v 3
+    }
+    |> shouldFail ("Expect: 1\nActual: 2", [])
---
+++ Persimmon/Persimmon.fs
@@ -0,0 +1,37 @@
+﻿module Persimmon
+
+type NonEmptyList<'T> = 'T * 'T list
+
+type ReturnType = UnitType | ValueType
+
+type AssertionResult<'T> =
+  | Success of 'T
+  | Failure of NonEmptyList<string>
+
+type TestBuilder(description: string) =
+  member __.Return(()) = Success ()
+  member __.ReturnFrom(x, _) = x
+  member __.Source(x: AssertionResult<unit>) = (x, UnitType)
+  member __.Source(x: AssertionResult<_>) = (x, ValueType)
+  member __.Bind(x, f: 'T -> AssertionResult<_>) =
+    match x with
+    | (Success x, _) -> f x
+    | (Failure (res1, rest1), UnitType) ->
+      match f (Unchecked.defaultof<'T>) with
+      | Success _ -> Failure (res1, rest1)
+      | Failure (res2, rest2) -> Failure (res1, rest1@(res2::rest2))
+    | (Failure xs, ValueType) -> Failure xs
+  member __.Delay(f: unit -> AssertionResult<_>) = f
+  member __.Run(f) = f ()
+
+let test description = TestBuilder(description)
+ 
+let inline checkWith returnValue expected actual =
+  if expected = actual then Success returnValue
+  else Failure (sprintf "Expect: %A\nActual: %A" expected actual, [])
+
+let failure msg = Failure (msg, [])
+let success v = Success v
+
+let check expected actual = checkWith actual expected actual
+let assertEquals expected actual = checkWith () expected actual
