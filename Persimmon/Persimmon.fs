a43a6de7af2302a35b026aabb6a5e1c8cc33c34c
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2014-10-28 17:47:34 +0900

    separate NonEmptyList type and module from Persimmon.fs

--- Persimmon/Persimmon.fs
+++ Persimmon/Persimmon.fs
@@ -1,23 +1,5 @@
 ﻿module Persimmon
 
-type NonEmptyList<'T> = 'T * 'T list
-
-[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
-module NonEmptyList =
-  let cons head tail : NonEmptyList<_> = (head, tail)
-  let singleton head : NonEmptyList<_> = (head, [])
-  let append (xs: NonEmptyList<_>) (ys: NonEmptyList<_>) : NonEmptyList<_> =
-    let x, xs = xs
-    let y, ys = ys
-    (x, (xs@(y::ys)))
-  let iter action (list: NonEmptyList<'T>) =
-    let head, tail = list
-    action head
-    List.iter action tail
-  let toList (list: NonEmptyList<'T>) =
-    let head, tail = list
-    [ yield head; yield! tail ]
-
 type ReturnType = UnitType | ValueType
 
 type AssertionResult<'T> =
