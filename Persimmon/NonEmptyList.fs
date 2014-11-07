c323a851fea763bc7c05891eb9cd961ae58686c6
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2014-11-07 22:14:43 +0900

    add signature file for NonEmptyList

--- Persimmon/NonEmptyList.fs
+++ Persimmon/NonEmptyList.fs
@@ -5,7 +5,8 @@ type NonEmptyList<'T> = 'T * 'T list
 [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
 module NonEmptyList =
   let head (xs: NonEmptyList<_>) = let head, _ = xs in head
-  let cons head tail : NonEmptyList<_> = (head, tail)
+  let make head tail : NonEmptyList<_> = (head, tail)
+  let cons head tail : NonEmptyList<_> = let second, tail = tail in (head, second::tail)
   let singleton head : NonEmptyList<_> = (head, [])
   let append (xs: NonEmptyList<_>) (ys: NonEmptyList<_>) : NonEmptyList<_> =
     let x, xs = xs
---
+++ Persimmon/NonEmptyList.fsi
@@ -0,0 +1,13 @@
+﻿namespace global
+
+type NonEmptyList<'T> = 'T * 'T list
+
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module NonEmptyList =
+  val head: NonEmptyList<'T> -> 'T
+  val make: 'T -> 'T list -> NonEmptyList<'T>
+  val cons: 'T -> NonEmptyList<'T> -> NonEmptyList<'T>
+  val singleton: 'T -> NonEmptyList<'T>
+  val append: NonEmptyList<'T> -> NonEmptyList<'T> -> NonEmptyList<'T>
+  val iter: ('T -> unit) -> NonEmptyList<'T> -> unit
+  val toList: NonEmptyList<'T> -> 'T list
