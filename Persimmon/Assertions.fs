f8ddf940ef9bd4cee32cff8162dfa50efa93f9c8
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2014-11-09 16:10:45 +0900

    add new assertions using new model

---
+++ Persimmon/Assertions.fs
@@ -0,0 +1,17 @@
+﻿[<AutoOpen>]
+module Persimmon.Assertions
+
+let fail message = NotPassed (Violated message)
+let pass value = Passed value
+
+let assertEquals expected actual =
+  if expected = actual then pass ()
+  else fail (sprintf "Expect: %A\nActual: %A" expected actual)
+
+let assertNotEquals notExpected actual =
+  if notExpected <> actual then pass ()
+  else fail (sprintf "Not Expected: %A\nActual: %A" notExpected actual)
+
+let assertPred pred =
+  if pred then pass ()
+  else fail "assert fail."
