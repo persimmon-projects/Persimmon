718f980f72a46a4bdb6d1886b0976b6fffd78429
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2014-11-11 07:40:24 +0900

    add Writable.fs
    eliminate the Writer from new model.
    we just use the TextWriter.

---
+++ Persimmon.Output/Writable.fs
@@ -0,0 +1,26 @@
+﻿namespace Persimmon.Output
+
+open System.IO
+open System.Xml
+
+type IWritable =
+  abstract WriteTo: writer:TextWriter -> unit
+
+module Writable =
+  let char (ch: char) =
+    { new IWritable with
+        member __.WriteTo(writer: TextWriter) =
+          writer.Write(ch)
+        }
+
+  let stringSeq (strs: string seq) =
+    { new IWritable with
+        member __.WriteTo(writer: TextWriter) = 
+          strs |> Seq.iter (writer.WriteLine)
+        }
+
+  let xdocument (xdoc: XmlDocument) =
+    { new IWritable with
+        member __.WriteTo(writer: TextWriter) =
+          xdoc.Save(writer)
+        }
