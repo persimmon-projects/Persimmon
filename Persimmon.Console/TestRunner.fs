46a23965f8f8cfdc437a8905075911b735751777
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2014-11-16 11:24:52 +0900

    implement aggregation the count of error tests

--- Persimmon.Console/TestRunner.fs
+++ Persimmon.Console/TestRunner.fs
@@ -28,7 +28,21 @@ let runTests (reporter: Reporter) (test: TestObject) =
       reporter.ReportProgress(result)
       result :> ITestResult
 
+let rec countErrors = function
+| ContextResult cr ->
+    cr.Children |> List.sumBy countErrors
+| TestResult tr ->
+    match tr with
+    | Break _ -> 1
+    | Done (_, res) ->
+        let typicalRes = AssertionResult.NonEmptyList.typicalResult res
+        match typicalRes with
+        | NotPassed (Violated _) -> 1
+        | NotPassed (Skipped _) -> 0
+        | Passed _ -> 0
+| EndMarker -> 0
+
 let runAllTests reporter (tests: TestObject list) =
   let rootResults = tests |> List.map (runTests reporter)
-  // todo : aggregate the count of error tests
-  { Errors = 0; ExecutedRootTestResults = rootResults }
+  let errors = rootResults |> List.sumBy countErrors
+  { Errors = errors; ExecutedRootTestResults = rootResults }
