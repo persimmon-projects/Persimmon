5b6916acf9e3279ff7cac6bc4416729ff8a2e5c1
pocketberserker <pocketberserker@gmail.com>
2014-11-18 20:29:43 +0900

    add AssemblyInfo

---
+++ Persimmon.Runner/AssemblyInfo.fs
@@ -0,0 +1,15 @@
+﻿namespace global
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyTitle("Persimmon.Runner")>]
+[<assembly: AssemblyProduct("Persimmon.Runner")>]
+[<assembly: Guid("EB676E7D-9D9D-47C2-A6DC-173B536641B1")>]
+
+[<assembly: AssemblyVersion("1.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0")>]
+[<assembly: AssemblyInformationalVersion("1.0.0-alpha")>]
+
+do ()
---
+++ Persimmon/AssemblyInfo.fs
@@ -0,0 +1,15 @@
+﻿namespace global
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyTitle("Persimmon")>]
+[<assembly: AssemblyProduct("Persimmon")>]
+[<assembly: Guid("F5EB6EEA-FA93-4F0D-9C23-60A91DB012DB")>]
+
+[<assembly: AssemblyVersion("1.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0")>]
+[<assembly: AssemblyInformationalVersion("1.0.0-alpha")>]
+
+do ()
