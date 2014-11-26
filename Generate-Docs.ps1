e3681ada2b748d322a51fad5f0940da426446cc7
pocketberserker <pocketberserker@gmail.com>
2014-11-26 15:25:39 +0900

    setup Japanese document

--- Generate-Docs.ps1
+++ Generate-Docs.ps1
@@ -1,1 +1,14 @@
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe .\docs\tools\Persimmon.Docs.proj /property:Configuration=Release /property:VisualStudioVersion=12.0 /target:rebuild
+$projectName = '.\docs\tools\Persimmon.Docs.proj'
+
+$project = cat $projectName
+$project = $project.Replace('generate.fsx', 'generate.ja.fsx')
+
+$projectNameJa = '.\docs\tools\Persimmon.Docs.ja.proj'
+$project | Out-File -Encoding UTF8 $projectNameJa
+
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe $projectNameJa /property:Configuration=Release /property:VisualStudioVersion=12.0 /target:rebuild
+
+rm $projectNameJa
+
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe $projectName /property:Configuration=Release /property:VisualStudioVersion=12.0 /target:build
+
