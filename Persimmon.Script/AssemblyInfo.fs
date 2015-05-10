3b2f941287a069b3aad2fc491f6bfa29dc2a9be6
pocketberserker <pocketberserker@gmail.com>
2015-05-11 08:13:55 +0900

    1.0.0-beta6

--- Persimmon.Script/AssemblyInfo.fs
+++ Persimmon.Script/AssemblyInfo.fs
@@ -9,6 +9,6 @@ open System.Runtime.InteropServices
 
 [<assembly: AssemblyVersion("1.0.0")>]
 [<assembly: AssemblyFileVersion("1.0.0")>]
-[<assembly: AssemblyInformationalVersion("1.0.0-beta5")>]
+[<assembly: AssemblyInformationalVersion("1.0.0-beta6")>]
 
 do ()
