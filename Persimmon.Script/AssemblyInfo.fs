94de7588a8d0f31bf9092df58cadcaa7c0c1a409
pocketberserker <pocketberserker@gmail.com>
2015-10-18 17:43:24 +0900

    1.0.0

--- Persimmon.Script/AssemblyInfo.fs
+++ Persimmon.Script/AssemblyInfo.fs
@@ -9,6 +9,6 @@ open System.Runtime.InteropServices
 
 [<assembly: AssemblyVersion("1.0.0")>]
 [<assembly: AssemblyFileVersion("1.0.0")>]
-[<assembly: AssemblyInformationalVersion("1.0.0-beta8")>]
+[<assembly: AssemblyInformationalVersion("1.0.0")>]
 
 do ()
--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,2 +1,2 @@
-### 1.0.0 - October 10 2015
+### 1.0.0 - October 18 2015
 * initial release
