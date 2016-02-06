a57a2ae8853c9edd06c1e3dbc85df05375a54afe
pocketberserker <pocketberserker@gmail.com>
2016-02-06 18:17:36 +0900

    output summary with multiple output forms

--- Persimmon.Console/Program.fs
+++ Persimmon.Console/Program.fs
@@ -32,24 +32,35 @@ let entryPoint (args: Args) =
         reporter.ReportProgress(TestResult.endMarker)
         reporter.ReportSummary(res.ExecutedRootTestResults)
         res.Errors
-  use output =
-    match args.Output with
-    | Some file -> new StreamWriter(file.FullName, false, Encoding.UTF8) :> TextWriter
-    | None -> Console.Out
+  let requireFileName, outputs =
+    let console = {
+      Writer = Console.Out
+      Formatter = Formatter.SummaryFormatter.normal watch
+    }
+    match args.Output, args.Format with
+    | (Some file, JUnitStyleXml) ->
+      let xml = {
+        Writer = new StreamWriter(file.FullName, false, Encoding.UTF8) :> TextWriter
+        Formatter = Formatter.XmlFormatter.junitStyle watch
+      }
+      (true, [console; xml])
+    | (Some file, Normal) ->
+      let file = {
+        Writer = new StreamWriter(file.FullName, false, Encoding.UTF8) :> TextWriter
+        Formatter = console.Formatter
+      }
+      (false, [console; file])
+    | (None, Normal) -> (false, [console])
+    | (None, JUnitStyleXml) -> (true, [])
   use error =
     match args.Error with
     | Some file -> new StreamWriter(file.FullName, false, Encoding.UTF8) :> TextWriter
     | None -> Console.Error
 
-  let requireFileName, formatter =
-    match args.Format with
-    | JUnitStyleXml -> (true, Formatter.XmlFormatter.junitStyle watch)
-    | Normal -> (false, Formatter.SummaryFormatter.normal watch)
-
   use reporter =
     new Reporter(
       new Printer<_>(progress, Formatter.ProgressFormatter.dot),
-      new Printer<_>(output, formatter),
+      new Printer<_>(outputs),
       new Printer<_>(error, Formatter.ErrorFormatter.normal))
 
   if args.Help then
