dec19c4c8c8918717d297b2f8c30617c06e890f0
pocketberserker <pocketberserker@gmail.com>
2016-08-14 05:42:54 +0900

    net20, net40, profile 259, netstandard16

--- src/Persimmon/Internals/TestCollector.fs
+++ src/Persimmon/Internals/TestCollector.fs
@@ -9,12 +9,28 @@ open Persimmon
 module private TestCollectorImpl =
 
   let publicTypes (asm: Assembly) =
+#if PCL || CORE_CLR
+    asm.ExportedTypes
+    |> Seq.filter (fun typ ->
+      let typ = typ.GetTypeInfo()
+      typ.IsClass && not typ.IsGenericTypeDefinition
+    )
+#else
     asm.GetTypes()
     |> Seq.filter (fun typ -> typ.IsPublic && typ.IsClass && not typ.IsGenericTypeDefinition)
+#endif
 
   let private publicNestedTypes (typ: Type) =
+#if PCL || CORE_CLR
+    typ.GetTypeInfo().DeclaredNestedTypes
+    |> Seq.choose (fun typ ->
+      if typ.IsNestedPublic && typ.IsClass && not typ.IsGenericTypeDefinition then
+        Some(typ.AsType())
+      else None)
+#else
     typ.GetNestedTypes()
     |> Seq.filter (fun typ -> typ.IsNestedPublic && typ.IsClass && not typ.IsGenericTypeDefinition)
+#endif
 
   /// Traverse test instances recursive.
   /// <param name="partialSuggest">Nested sequence children is true: symbol naming is pseudo.</param>
@@ -66,24 +82,45 @@ module private TestCollectorImpl =
   /// Retreive test object via target property, and traverse.
   let private collectTestsFromProperty (p: PropertyInfo) =
     fixupAndCollectTests (p.GetValue(null, null), p.Name, None)
-  
+
   /// Retreive test object via target method, and traverse.
   let private collectTestsFromMethod (m: MethodInfo) =
     fixupAndCollectTests (m.Invoke(null, [||]), m.Name, None)
-  
+
   /// Retreive test object via target type, and traverse.
   let rec collectTests (typ: Type) = seq {
     // For properties (value binding):
     yield!
-      typ.GetProperties(BindingFlags.Static ||| BindingFlags.Public)
+      typ
+#if PCL || CORE_CLR
+        .GetTypeInfo().DeclaredProperties
+      |> Seq.filter (fun x ->
+        let m = x.GetMethod
+        m.IsStatic && m.IsPublic
+      )
+      // Ignore setter only property / indexers
+      |> Seq.filter (fun p -> p.CanRead && (p.GetMethod <> null) && (p.GetIndexParameters() |> Array.isEmpty))
+#else
+        .GetProperties(BindingFlags.Static ||| BindingFlags.Public)
       // Ignore setter only property / indexers
       |> Seq.filter (fun p -> p.CanRead && (p.GetGetMethod() <> null) && (p.GetIndexParameters() |> Array.isEmpty))
+#endif
       |> Seq.collect collectTestsFromProperty
     // For methods (function binding):
     yield!
-      typ.GetMethods(BindingFlags.Static ||| BindingFlags.Public)
+      typ
+#if PCL || CORE_CLR
+        .GetTypeInfo().DeclaredMethods
+      // Ignore getter methods / open generic methods / method has parameters
+      |> Seq.filter (fun m ->
+        m.IsStatic && m.IsPublic &&
+        not m.IsSpecialName && not m.IsGenericMethodDefinition && (m.GetParameters() |> Array.isEmpty)
+      )
+#else
+        .GetMethods(BindingFlags.Static ||| BindingFlags.Public)
       // Ignore getter methods / open generic methods / method has parameters
       |> Seq.filter (fun m -> not m.IsSpecialName && not m.IsGenericMethodDefinition && (m.GetParameters() |> Array.isEmpty))
+#endif
       |> Seq.collect collectTestsFromMethod
     // For nested modules:
     for nestedType in publicNestedTypes typ do
