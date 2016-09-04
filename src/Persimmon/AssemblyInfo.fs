e67ef06d752742324c1a1869c3efe6507e6a9675
pocketberserker <pocketberserker@gmail.com>
2016-09-04 13:36:33 +0900

    Bump version to 1.2.0

--- src/Persimmon/AssemblyInfo.fs
+++ src/Persimmon/AssemblyInfo.fs
@@ -8,10 +8,10 @@ open System.Runtime.InteropServices
 [<assembly: InternalsVisibleToAttribute("Persimmon.Tests")>]
 [<assembly: GuidAttribute("F5EB6EEA-FA93-4F0D-9C23-60A91DB012DB")>]
 [<assembly: AssemblyProductAttribute("Persimmon")>]
-[<assembly: AssemblyVersionAttribute("1.1.0")>]
-[<assembly: AssemblyFileVersionAttribute("1.1.0")>]
-[<assembly: AssemblyInformationalVersionAttribute("1.1.0")>]
+[<assembly: AssemblyVersionAttribute("1.2.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.2.0")>]
+[<assembly: AssemblyInformationalVersionAttribute("1.2.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.1.0"
+    let [<Literal>] Version = "1.2.0"
