f6e29f783d89020cdc889270d34c594114abbfc2
hafuu <hafuu46@gmail.com>
2017-11-16 15:42:01 +0900

    implement Equals, GetHashCode and ToString to AssertionResult

---
+++ tests/Persimmon.Tests/AssertionResultTest.fs
@@ -0,0 +1,26 @@
+﻿namespace Persimmon.Tests
+
+open Persimmon
+open UseTestNameByReflection
+
+module AssertionResultTest =
+  open Persimmon.TestResult
+
+  let ``should equal`` = parameterize {
+    source [
+      (Passed "a"), (Passed "a"), true
+      (Passed "a"), (Passed "b"), false
+
+      (Passed null), (Passed null), true
+      (Passed "a"), (Passed null), false
+
+      (NotPassed (Some 0, Skipped "skip")), (NotPassed (Some 0, Skipped "skip")), true
+      (NotPassed (Some 0, Skipped "skip")), (NotPassed (Some 1, Skipped "skip")), false
+      (NotPassed (Some 0, Skipped "skip")), (NotPassed (Some 0, Skipped "SKIP")), false
+
+      (Passed "a"), (NotPassed (Some 0, Skipped "skip")), false
+    ]
+    run (fun (left, right, expected) -> test {
+      do! (left = right) |> assertEquals expected
+    })
+  }
